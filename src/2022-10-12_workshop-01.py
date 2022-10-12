e131fff2790b5c29fd2f64ed554bd483b9d094c3
entropycrusher <tgraettinger@discoverycorpsinc.com>
2022-10-12 15:15:15 -0400

    Update 2022-10-12_workshop-01.py
    ... read different files...

--- src/2022-10-12_workshop-01.py
+++ src/2022-10-12_workshop-01.py
@@ -13,9 +13,18 @@ print('\n' + 'Hi ' + your_name + '!! ' + welcome_text)
 
 # Loading data (dgk_train.csv)
 ## Note that the following are constants, denoted by ALL CAPS
-DATA_FILE_NAME   = "dgk_train.csv"
+DATA_FILE_NAME   = "bank-telemarketing_train.csv"
 DATA_FOLDER_NAME = "../data/"
 
 import pandas as pd
 working = pd.read_csv(DATA_FOLDER_NAME + DATA_FILE_NAME)
 
+# Your turn, do the same for the census-income dataset...
+DATA_FILE_NAME   = "census-income_train.csv"
+ELEMENT_NAMES    = ["age", "work", "fnlw", "edu", "edun", "marit", "occ", "rel", "race", "sex", "capg", "capl", "hours", "nativ", "ovr50"]
+working = pd.read_csv(DATA_FOLDER_NAME + DATA_FILE_NAME, header=None, names=ELEMENT_NAMES)
+
+# Your turn, do the same for the bank-telemarketing dataset...
+DATA_FILE_NAME   = "bank-telemarketing_train.csv"
+FILE_SEPARATOR   = ";"
+working = pd.read_csv(DATA_FOLDER_NAME + DATA_FILE_NAME, sep=FILE_SEPARATOR)
