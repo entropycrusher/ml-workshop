cef0aa0686d65e0e0a0b25bd7a7fc144adf6a167
entropycrusher <tgraettinger@discoverycorpsinc.com>
2022-10-12 15:03:22 -0400

    Update 2022-10-12_workshop-01.py
    ... added a few more lines...

--- src/2022-10-12_workshop-01.py
+++ src/2022-10-12_workshop-01.py
@@ -11,3 +11,11 @@ print(welcome_text)
 your_name = 'Tim'
 print('\n' + 'Hi ' + your_name + '!! ' + welcome_text)
 
+# Loading data (dgk_train.csv)
+## Note that the following are constants, denoted by ALL CAPS
+DATA_FILE_NAME   = "dgk_train.csv"
+DATA_FOLDER_NAME = "../data/"
+
+import pandas as pd
+working = pd.read_csv(DATA_FOLDER_NAME + DATA_FILE_NAME)
+
