31fdad91a64d8fe8839b95f0430baa2e001e3733
entropycrusher <tgraettinger@discoverycorpsinc.com>
2022-10-12 14:57:04 -0400

    Create 2022-10-12_workshop-01.py
    initial version of my first script...

---
+++ src/2022-10-12_workshop-01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 14:47:25 2022
+
+@author: tgrae
+"""
+
+welcome_text = 'Welcome to the Machine Learning Workshop!'
+print(welcome_text)
+
+your_name = 'Tim'
+print('\n' + 'Hi ' + your_name + '!! ' + welcome_text)
+
